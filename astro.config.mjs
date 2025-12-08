d018f52debe685b995904978851ace14087de3ef
CristhianZ2022 <cris12x12@gmail.com>
2025-12-08 05:12:46 -0500

    Vercel to render build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
