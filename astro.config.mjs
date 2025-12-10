224c68d6e4f366dfd9f95f1424f2da63ded8be1a
CristhianZ2022 <cris12x12@gmail.com>
2025-12-09 23:57:58 -0500

    Library & package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel/serverless";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,7 +13,10 @@ export default defineConfig({
   },
 
   output: "server",
+
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+
+  integrations: [react()],
 });
